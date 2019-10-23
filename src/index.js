4ff9eb99d7d96148eaefe5b748b7f0879c601f57
Christiano Milfont <cmilfont@gmail.com>
2019-10-23 14:44:09 -0300

    melhorando o cache do firebase e colocando o sw para o workbox funcionar local

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
